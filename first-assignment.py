b460dc0d8c01a8041be4ca2b93106f9f88f0be62
KD <kdkahlo@gmail.com>
2024-07-17 13:55:46 +0200

    casting practice

---
+++ first-assignment.py
@@ -0,0 +1,31 @@
+"""print("Hello World")
+
+print("Kellie is learning Python", 14)
+
+print ("I can print:", 10/2)
+
+print ( "e.g. some math:", 10*2)
+
+my_variable = "42"
+
+answer = "The Answer is" + " " +  my_variable
+
+ print(answer)"""
+
+first_name = "Kellie"
+last_name = "Dixon"
+age = 46
+print("My name is:", first_name, last_name, "and I am", age, "years old!")
+
+num = 3.14
+num = int(num)
+num = str(num)
+num = bool(num)
+num = str(num)
+num = float(num)
+
+print(num)
+
+
+
+
