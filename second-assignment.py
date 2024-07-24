46daf7bffd8e6a21d2b2c9e4c5e56561d06f4335
KD <kdkahlo@gmail.com>
2024-07-24 09:18:56 +0200

    adding input to define a variable

--- second-assignment.py
+++ second-assignment.py
@@ -1,4 +1,11 @@
-num1 = 1
-num2 = 6
-name = "Kellie"
+# num1 = 1
+# num2 = 6
+# name = "Kellie"
+
+# you can use input to take in an input and give an output based on the input. 
+# in the example below, the input will ask "What is your name?"
+# you then would have to type a response 
+# you will get back the print output with what was typed defined as the name variable.
+
+name = input("What is your name? ")
 print("Hello", name , "Bye", name)
