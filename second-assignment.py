ef163f4f71b706390768e3942c800fc0dedd7a2c
KD <kdkahlo@gmail.com>
2024-07-24 10:05:28 +0200

    simple chatbot to display age fun fact

--- second-assignment.py
+++ second-assignment.py
@@ -3,9 +3,21 @@
 # name = "Kellie"
 
 # you can use input to take in an input and give an output based on the input. 
-# in the example below, the input will ask "What is your name?"
+# in the example below, the input defines the name variable and will give an output requesting input with "What is your name?"
 # you then would have to type a response 
 # you will get back the print output with what was typed defined as the name variable.
 
-name = input("What is your name? ")
-print("Hello", name , "Bye", name)
+# name = input("What is your name? ")
+# print("Hello", name , "Bye", name)
+
+
+
+# age = input("What is your age? ")
+# age = int(age)
+
+# # print(type(age))
+# print(age)
+
+a = 5 + 5
+print ("a is")
+
---
+++ simple-chatbot.py
@@ -0,0 +1,12 @@
+#Assignments for today
+#Create a Simple Chatbot
+#Write a program that takes & stores the following data from the person using the program via input():
+# the first name and the year of birth
+#Afterwards the program should output a personal greeting and calculate how old the respective person will be in the year 2050.
+name = input("What is your name? ")
+print("Hello", name)
+
+age = int(input("What year were you born? "))
+age = 2050 - age
+
+print ("Here is a fun fact for you ", name, "." , "In 2050 you will be ", age , "years old!")
