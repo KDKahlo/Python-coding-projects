400b22f243d92ecf836507259d820a4e9001be86
KD <kdkahlo@gmail.com>
2024-07-31 07:56:50 +0200

    while loop completed

--- 24-07-homework.py
+++ 24-07-homework.py
@@ -1,9 +1,17 @@
 #Exercise 14
-number = int(input("Give me any number: "))
+# number = int(input("Give me any number: "))
 
-if number > 18:
-    print("number is bigger than 18")
-elif number <= 17:
-    print("number is smaller than or equal to 17")
-elif number < 100:
-    print("number is smaller than 100")
+# if number > 18:
+#     print("number is bigger than 18")
+# elif number <= 17:
+#     print("number is smaller than or equal to 17")
+# elif number < 100:
+#     print("number is smaller than 100")
+    
+    
+#Exercise 15
+
+number = 5
+
+while number < 10:
+    print(f"{number} is smaller than 10")
