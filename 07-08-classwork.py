c335dfcb3cbdbea3dd312475790b1ff7ed683c4f
KD <kdkahlo@gmail.com>
2024-08-07 11:48:53 +0200

    updates and append to data dictionary

--- 07-08-classwork.py
+++ 07-08-classwork.py
@@ -1,6 +1,15 @@
 data = { "people": [
     {"name":"Alice", "age":30, "email":"alice@email.com"},
     {"name":"Bob", "age":40, "email":"bob@email.com"},
-    {"name":"Charlie", "age":50}
-    
+    {"name":"Charlie", "age":50}   
 ]}
+
+print(data["people"][2])
+data["people"][2]["email"] = "charlie@email.com"
+print(data["people"][2])
+
+data["people"][1]["age"] = "41"
+print(data["people"][1])
+
+data["people"].append({"name": "Dorothy", "age": 27, "email": "doro@email.com"})
+print(data)
