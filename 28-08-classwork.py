c7f8ee47b484d38ee3ff07ac0f3cbb4cb35234e3
KD <kdkahlo@gmail.com>
2024-09-04 10:58:07 +0200

    BankAccount class created

--- 28-08-classwork.py
+++ 28-08-classwork.py
@@ -7,3 +7,10 @@ galaxies = [1000000, 2300000, 500000]  # Example list of galaxies with star coun
 print("there are ",count_all_stars(galaxies), "stars in the galaxy")  # This should print the sum of all stars
 
 
+class BankAccount:
+    def __init__(self, balance):
+        self.__balance = balance #Private attribute
+        
+    # Public method to access private attribute
+    def get_balance(self):
+        return self.__balance
